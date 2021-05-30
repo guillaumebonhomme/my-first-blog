5b1db7676223ff7b715952646ed79814569ee220
GuillaumeBonhomme <guillaume.bonhomme@bureauxlocaux.com>
2021-05-30 16:12:10 +0200

    Add missing content in files

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+#application = get_wsgi_application()
+
+
 import os
+import sys
+
+path = '/home/guibo/guibo.eu.pythonanywhere.com'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
 application = get_wsgi_application()
